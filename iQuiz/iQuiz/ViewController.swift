c102b2d4dd7c9b1d03a6ed15296776756f060718
Leonardo Veras <andradeveras96@gmail.com>
2024-07-02 17:04:08 -0300

    Criando a tela principal
    Inserindo a imageview e um button

---
+++ iQuiz/iQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by Leonardo de Andrade Veras on 02/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
